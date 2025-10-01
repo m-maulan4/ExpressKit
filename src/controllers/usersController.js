5b83a51756ece400b19a234c88e13b740d3abfb0
Muammar Maulana <muammar676@gmail.com>
2025-10-01 16:22:48 +0700

    modified:   src/controllers/usersController.js

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,5 +2,5 @@ import { userModel } from "../models/usersModel.js";
 
 export const getUsers = async (req, res) => {
   const users = await userModel.findAll();
-  return res.send(users);
+  res.send(users);
 };
